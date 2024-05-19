8c504e75ec78c153c383ae3ceda31b74e9e449e9
Siddhartha Chikkavarapu <siddu_chikkavarapu@Siddharthas-MacBook-Pro.local>
2024-05-19 11:25:32 -0500

    intial commit

---
+++ src/test/java/com/prasquare/food/orders/foodorders/FoodOrdersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prasquare.food.orders.foodorders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodOrdersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
